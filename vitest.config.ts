d23b609ad0d7a4468c9d6f2e11066daa4509b3a0
Matteo Vivona <matteo.vivona@me.com>
2022-12-16 14:59:11 +0100

    ci: add coveralls

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import {defineConfig} from "vitest/config";
 
 export default async () => {
   return defineConfig({
@@ -6,22 +6,22 @@ export default async () => {
       clearMocks: true,
       silent: true, // we disable console log because we have some test that are expected to throw errors
       outputFile: {
-        junit: 'junit.xml',
+        junit: "junit.xml",
       },
       coverage: {
-        reporter: ['html', 'text', 'cobertura', 'json'],
-        provider: 'istanbul',
-        include: ['src/**/*.{ts,tsx}'],
+        reporter: ["lcov", "html"],
+        provider: "istanbul",
+        include: ["src/**/*.{ts,tsx}"],
         exclude: [
-          'src/**/*.test.{ts,tsx}',
+          "src/**/*.test.{ts,tsx}",
           // Root files only combine existing functions. We can apply functional tests, but not unit tests here
-          'src/{index,resolvers,schema,rest-routes}.ts',
+          "src/{index,resolvers,schema,rest-routes}.ts",
           // Schema definition has no functions
-          'src/**/*.schema.{ts,tsx}',
+          "src/**/*.schema.{ts,tsx}",
           // Database migrations have no business logic to test
-          'src/database/**/*.{ts,tsx}',
+          "src/database/**/*.{ts,tsx}",
           // Types have no business logic to test
-          'src/types/**/*.{ts,tsx}',
+          "src/types/**/*.{ts,tsx}",
         ],
       },
     },
